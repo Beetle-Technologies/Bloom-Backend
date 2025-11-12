81ef26f28dbacd105b09e062f710f7efbd8f3c84
Daniel Brai <danielbrai.dev@gmail.com>
2025-11-12 08:59:59 +0100

    chore(misc): Update GID body

--- app/src/domain/schemas/miscellaneous.py
+++ app/src/domain/schemas/miscellaneous.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic import BaseModel, Field
 
 
@@ -6,6 +8,6 @@ class GenerateGIDRequest(BaseModel):
     Schema for the request body to generate a new GID.
     """
 
-    resource_type: str = Field(
-        ..., description="The resource type for the GID (e.g., 'Account', 'Product', 'ProductItem')"
+    resource_type: Literal["Product", "ProductItem"] = Field(
+        ..., description="The resource type for the GID (e.g., 'Product', 'ProductItem')"
     )
